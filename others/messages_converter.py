6959c058ee6b479acfd6da9e14f9e19257dfb065
Antonio Rodriguez <rodriguez.ant@hotmail.com>
2024-03-04 17:51:37 +0100

    Adding prompt conversion utility

---
+++ others/messages_converter.py
@@ -0,0 +1,28 @@
+#Simple prompt converter from Anthropic Claude Text Completions API to Messages API
+#for details check the documentation here: https://docs.aws.amazon.com/bedrock/latest/userguide/model-parameters-anthropic-claude-messages.html
+
+import json, sys
+
+input_prompt = (sys.argv[1]).replace('//', '/')
+
+if input_prompt:
+    system_prompt = input_prompt.split("Human:")[0]
+    nosystem_prompt = input_prompt.split("Human:")[1]
+    user_prompt = nosystem_prompt.split("Assistant:")[0]
+    assistant_prompt = nosystem_prompt.split("Assistant:")[1]
+
+    output_prompt = []
+    if system_prompt:
+        output_prompt.append({"role": "system", "content": system_prompt})
+
+    if user_prompt:
+        output_prompt.append({"role": "user", "content": user_prompt})
+    
+    if assistant_prompt:
+        output_prompt.append({"role": "assistant", "content": assistant_prompt})
+
+    print(f'\n{json.dumps(output_prompt, ensure_ascii=False)}')
+    print('\nFor more info check the documentation here: https://docs.aws.amazon.com/bedrock/latest/userguide/model-parameters-anthropic-claude-messages.html')
+
+else:
+    raise ValueError("Please pass an input prompt between double quotes as the argument.")
